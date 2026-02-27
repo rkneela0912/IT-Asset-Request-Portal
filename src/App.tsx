5faad683d5a21c408f389d951b63a5234f0aae75
ranjith neela <iamranjithneela@gmail.com>
2026-02-27 17:32:03 -0500

    Wire up all 5 routes: Home, Catalog, RequestForm, StatusTracker, AdminDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import About from './pages/About'
+import Catalog from './pages/Catalog'
+import RequestForm from './pages/RequestForm'
+import StatusTracker from './pages/StatusTracker'
+import AdminDashboard from './pages/AdminDashboard'
 
 export default function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/"        element={<Home />}           />
+        <Route path="/catalog" element={<Catalog />}        />
+        <Route path="/request" element={<RequestForm />}    />
+        <Route path="/status"  element={<StatusTracker />}  />
+        <Route path="/admin"   element={<AdminDashboard />} />
       </Routes>
     </Layout>
   )
